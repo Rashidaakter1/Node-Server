7bb32292d30d4c5160c8fbc6155a391ca024274d
Rashida1akter <rashidaakterchadni@gamil.com>
2024-08-20 14:28:57 +0600

    first commit

---
+++ src/app/server.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  await mongoose.connect(config.database__url as string);
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+    console.log(`conteced db`);
+  });
+}
+
+main();
